8cd038052203e62ad0f4ea1294051e57b846dfda
Spencer Smolen <7726614-kriipke@users.noreply.gitlab.com>
2024-03-19 07:44:20 -0400

    Created separate logic for stage and prod branches in github actions and kustomizations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
-import { toast } from "react-toastify";
-import { Link } from "react-router-dom";
-import useStore from "../store";
-import Spinner from "./Spinner";
-import { logoutUserFn } from "../api/authApi";
-import { useMutation } from "@tanstack/react-query";
-import { Menu } from 'antd';
-import type { MenuProps } from "antd";
+import { toast } from 'react-toastify'
+import { Link } from 'react-router-dom'
+import useStore from '../store'
+import Spinner from './Spinner'
+import { logoutUserFn } from '../api/authApi'
+import { useMutation } from '@tanstack/react-query'
+import { Menu } from 'antd'
+import type { MenuProps } from 'antd'
+import React from 'react'
 
 const Header = () => {
-  const store = useStore();
-  const user = store.authUser;
+  const store = useStore()
+  const user = store.authUser
 
   //const items: MenuProps["items"] = [
   //  {
@@ -22,96 +23,95 @@ const Header = () => {
   //  },
   //];
 
-
-
   const { mutate: logoutUser } = useMutation(() => logoutUserFn(), {
     onMutate(variables) {
-      store.setRequestLoading(true);
+      store.setRequestLoading(true)
     },
     onSuccess(data) {
-      store.setRequestLoading(false);
-      toast.success("Successfully logged out", {
-        position: "top-right",
-      });
-      document.location.href = "/login";
+      store.setRequestLoading(false)
+      toast.success('Successfully logged out', {
+        position: 'top-right',
+      })
+      document.location.href = '/login'
     },
     onError(error: any) {
-      store.setRequestLoading(false);
-      store.setAuthUser(null);
-      document.location.href = "/login";
+      store.setRequestLoading(false)
+      store.setAuthUser(null)
+      document.location.href = '/login'
     },
-  });
-
+  })
 
   const handleLogout = () => {
-    logoutUser();
-  };
+    logoutUser()
+  }
 
-  const userMenu: MenuProps["items"] = ((user) => {
-    if (user == null) { 
+  const userMenu: MenuProps['items'] = (user => {
+    if (user == null) {
       return [
         {
           label: <a href="/register">Register</a>,
-          key: "register",
+          key: 'register',
         },
         {
           label: <a href="/login">Login</a>,
-          key: "login",
+          key: 'login',
         },
-      ];
+      ]
     } else {
       return [
         {
           label: <a href="/">Home</a>,
-          key: "home",
+          key: 'home',
         },
         {
           label: <a href="/profile">Profile</a>,
-          key: "profile",
+          key: 'profile',
         },
         {
-          label: <li className="cursor-pointer" onClick={handleLogout}>Logout</li>,
-          key: "logout",
+          label: (
+            <li className="cursor-pointer" onClick={handleLogout}>
+              Logout
+            </li>
+          ),
+          key: 'logout',
         },
-      ];
-
+      ]
     }
-  })(user);
+  })(user)
 
-
-  const onClick: MenuProps["onClick"] = (e: any) => {
-    console.log("click ", e);
+  const onClick: MenuProps['onClick'] = (e: any) => {
+    console.log('click ', e)
     store.currenCluster(e.key)
-  };
-
+  }
 
   return (
     <>
       <header className="bg-white h-15">
-
         <nav className="h-full flex justify-between container items-center">
-
           <div className="flex">
-            <Link to="/console" className="site-header text-ct-dark-600 text-2xl font-semibold">
-              <img className="site-logo" height="32" width="32" src="/logo.png" />
+            <Link
+              to="/console"
+              className="site-header text-ct-dark-600 text-2xl font-semibold"
+            >
+              <img
+                className="site-logo"
+                height="32"
+                width="32"
+                src="/logo.png"
+              />
             </Link>
           </div>
           <div className="flex">
-            <Menu
-              selectedKeys={"home"}
-              mode="horizontal"
-              items={userMenu}
-            />
+            <Menu selectedKeys={'home'} mode="horizontal" items={userMenu} />
           </div>
         </nav>
-
       </header>
 
       <div className="pt-4 pl-2 bg-ct-blue-600 fixed">
         {store.requestLoading && <Spinner color="text-ct-yellow-600" />}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
